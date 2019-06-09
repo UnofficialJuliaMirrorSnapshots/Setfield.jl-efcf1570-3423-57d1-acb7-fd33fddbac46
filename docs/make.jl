47ae658d7031649278b20aba9f237544f5603693
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-06-09 04:51:22 +0000

    Snapshot of branch master taken on 2019-06-09T00:51:21.658-04:00 from "https://github.com/UnofficialJuliaMirror/Setfield.jl-efcf1570-3423-57d1-acb7-fd33fddbac46"

--- docs/make.jl
+++ docs/make.jl
@@ -1,2 +1,15 @@
-include("build.jl")
-include("deploy.jl")
+using Setfield, Documenter
+
+makedocs(
+         modules = [Setfield],
+         sitename = "Setfield.jl",
+         pages = [
+            "Introduction" => "intro.md",
+            "Docstrings" => "index.md",
+            ],
+        strict = true,  # to exit with non-zero code on error
+        )
+
+deploydocs(
+    repo = "github.com/jw3126/Setfield.jl.git",
+)
